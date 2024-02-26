e756a358dcb6d32733868e433d98b2bcba1ab822
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-26 19:01:34 +0500

    commands added

--- 26-2-24/app.js
+++ 26-2-24/app.js
@@ -4,40 +4,19 @@
 // console.log(messi);
 
 
-// var totalMarks = (500)
-// console.log(totalMarks);
+var totalMarks = (500)
+console.log(totalMarks);
 
-// var physicsMarks = +prompt("Your Physics marks ...")
-// var mathsMarks = +prompt("Your Maths marks ...")
-// var chemistryMarks = +prompt("Your Chemistry marks ...")
-// var englishMarks = +prompt("Your English marks ...")
-// var bioMarks = +prompt("Your Biology marks ...")
-// var obtainedMarks = (physicsMarks+mathsMarks+chemistryMarks+englishMarks+bioMarks)
+var physicsMarks = +prompt("Your Physics marks ...")
+var mathsMarks = +prompt("Your Maths marks ...")
+var chemistryMarks = +prompt("Your Chemistry marks ...")
+var englishMarks = +prompt("Your English marks ...")
+var bioMarks = +prompt("Your Biology marks ...")
+var obtainedMarks = (physicsMarks+mathsMarks+chemistryMarks+englishMarks+bioMarks)
 
-// document.write("Your total marks are " + totalMarks + " and your obtained marks are " + obtainedMarks)
+document.write("Your total marks are " + totalMarks + " and your obtained marks are " + obtainedMarks)
 
-// document.write(" and your percentage is " + (obtainedMarks/totalMarks*100) +"%")
-
-
-
-
-
-
-
-
-// ye ismail ne tatti ki he
-// ye ismail ne tatti ki he
-// ye ismail ne tatti ki he
-
-
-
-
-// var totalMarks = (500);
-// console.log(totalMarks);
-
-// var englishMarks = +prompt("your total marks are...")
-// var bioMarks = +prompt("your total marks are...")
-// var chemistryMarks = +prompt()
+document.write(" and your percentage is " + (obtainedMarks/totalMarks*100) +"%")
 
 
 
