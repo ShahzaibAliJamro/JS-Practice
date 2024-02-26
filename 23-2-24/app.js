4614c00870320a2c88f22e8235d24870711ea19c
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-26 18:59:15 +0500

    commands added

---
+++ 23-2-24/app.js
@@ -0,0 +1,56 @@
+// var num1 = +prompt("Enter 1st number");
+// var num2 = +prompt("Enter 2nd number");
+// var num3 = +prompt("Enter 3rd number");
+// var num4 = +prompt("Enter 4th number");
+
+// console.log(num1 + num2 + num3 + num4)
+
+
+// var num1 = +prompt('Enter first number')
+// var num2 = +prompt('Enter second number')
+// var num3 = +prompt('Enter third number')
+// var num4 = +prompt('Enter fourth number')
+
+// console.log(num1+num2+num3+num4);
+
+
+
+
+// var num10 = +prompt("enter a number");
+// var num11 = +prompt("enter another number");
+// console.log(num10 > num11);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = prompt('enter first number')
+// var num2 = prompt('enter second number')
+
+
+
+console.log(10 !== 10);
+
+
+
+var userName = document.write("<h1>HELLO JS</h1>")
+
+
+
+
---
+++ 26-2-24/app.js
@@ -0,0 +1,63 @@
+// var ronaldo = "10" >= "10" || "10" > "10";
+// // console.log(ronaldo);
+// var messi = 8 >= 5;
+// console.log(messi);
+
+
+// var totalMarks = (500)
+// console.log(totalMarks);
+
+// var physicsMarks = +prompt("Your Physics marks ...")
+// var mathsMarks = +prompt("Your Maths marks ...")
+// var chemistryMarks = +prompt("Your Chemistry marks ...")
+// var englishMarks = +prompt("Your English marks ...")
+// var bioMarks = +prompt("Your Biology marks ...")
+// var obtainedMarks = (physicsMarks+mathsMarks+chemistryMarks+englishMarks+bioMarks)
+
+// document.write("Your total marks are " + totalMarks + " and your obtained marks are " + obtainedMarks)
+
+// document.write(" and your percentage is " + (obtainedMarks/totalMarks*100) +"%")
+
+
+
+
+
+
+
+
+// ye ismail ne tatti ki he
+// ye ismail ne tatti ki he
+// ye ismail ne tatti ki he
+
+
+
+
+// var totalMarks = (500);
+// console.log(totalMarks);
+
+// var englishMarks = +prompt("your total marks are...")
+// var bioMarks = +prompt("your total marks are...")
+// var chemistryMarks = +prompt()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
