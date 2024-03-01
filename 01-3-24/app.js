9f26526c3cb30526b35cd3811b6ce29757ca912f
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-02 00:22:01 +0500

    commands added

---
+++ 01-3-24/app.js
@@ -0,0 +1,28 @@
+// function sum (num1 , num2){
+//     console.log(num1 + num2);
+// }
+// sum(149 , 51);
+
+// function myName (){
+//     console.log('My name is Shahzaib');
+//     return 20 + 20 ;
+// }
+
+// var name1 = myName();
+// console.log(name1);
+
+
+// function percentageCalculator (marks1 , marks2, marks3, marks4, marks5){
+//     var totalMarks = 500;
+//     var obtainedMarks = marks1 + marks2 + marks3 + marks4 + marks5 ;
+//     return obtainedMarks / totalMarks * 100;
+// }
+
+
+var heading = document.querySelector(".helloabbu");
+console.log(heading.innerHTML);
+
+
+function abbuBeta (){
+heading.innerHTML = "Ji beta" ;
+}
