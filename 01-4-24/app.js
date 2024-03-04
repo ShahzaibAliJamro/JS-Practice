60e6eac346dcb389da773e1dc117a5bafbc33d7f
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-04 13:46:55 +0500

    commands added

---
+++ 01-4-24/app.js
@@ -0,0 +1,41 @@
+// var head = document.querySelector(".apple");
+// function changed(){
+//     if (head.innerHTML === "Hello World") {
+//         head.innerHTML = "Changed through JS";
+//     }else {
+//         head.innerHTML = "Hello World";
+//     }
+// }
+
+
+// var input = document.querySelector('#input');
+// function inputSave() {
+//     console.log(input.value);
+//     input.value = '';
+// }
+
+
+
+// var input = document.querySelector("#input")
+// function inputSave() {
+//     console.log(input.value);
+//     input.value = ""
+// }
+
+
+
+
+
+var firstName = "ismail"
+var lastName = "ali"
+console.log(`my name is ${firstName} ${lastName}`);
+
+
+
+
+
+
+
+
+
+
