6eddeaebfeeac037394760b79ccc3d8e77fe7bef
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-06 18:30:48 +0500

    commands added

--- 06-3-24/app.js
+++ 06-3-24/app.js
@@ -44,16 +44,34 @@ var input1 = document.querySelector('#input1');
 var input2 = document.querySelector('#input2');
 var input3 = document.querySelector('#input3');
 var input4 = document.querySelector('#input4');
-var marks = document.querySelector('#marks')
-var Grade = document.querySelector('#grade')
-var percent = document.querySelector('#percentage')
+var marks = document.querySelector('#marks');
+var Grade = document.querySelector('#grade');
+var percent = document.querySelector('#percentage');
 function getValue() {
     var totalMarks = 400;
     var obtainedMarks = +input1.value + +input2.value + +input3.value + +input4.value;
     var percentage = obtainedMarks/totalMarks*100;
     marks.innerHTML = obtainedMarks;
     percent.innerHTML = percentage;
-
+    if(percentage<33){
+        Grade.innerHTML = "FAIL";
+    }else if(percentage>=33 && percentage<40){
+        Grade.innerHTML = "E";
+    }else if(percentage>=40 && percentage<50){
+        Grade.innerHTML = "D";
+    }else if(percentage>=50 && percentage<60){
+        Grade.innerHTML = "C";
+    }else if(percentage>=60 && percentage<70){
+        Grade.innerHTML = "B";
+    }else if(percentage>=70 && percentage<80){
+        Grade.innerHTML = "A";
+    }else if(percentage>=80 && percentage<90){
+        Grade.innerHTML = "A+";
+    }else if(percentage>=90 && percentage<=100){
+        Grade.innerHTML = "A++";
+    }else{
+        Grade.innerHTML = "A++";
+    }
 }
 
 
