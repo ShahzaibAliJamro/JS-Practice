186818327563f6665f28e8a7cf921869ef3424f3
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-08 21:48:18 +0500

    commands added

---
+++ 08-3-24/app.js
@@ -0,0 +1,68 @@
+// const footballPlayers = ['Rulando','Mossi','Neymar','Modric',15,'Lewandowski',['Bruno','Rashford','Foden']]
+// console.log(footballPlayers[0]);
+// console.log(footballPlayers[6][2]);
+
+
+
+
+// cricketPlayer.push(ruladno);end me dega
+// cricketPlayer.pop();end se nikalega
+// cricketPlayer.unshift("rulando");saamne se dega
+// cricketPlayer.shift();saamne se nikalega
+// console.log(`${cricketPlayer[3][2]}`);
+
+
+
+
+
+// const cricketPlayer = ['Virat' , 'babar' , "rizwan" , ['saim', 'sai' ,'sa']]
+
+// cricketPlayer[]
+
+
+
+
+
+// const fruits = ["mango" , "banana", "pineapple", "apple", "guava","papita","kheera","gaajar","mooli","pomegrenade","grapes" ];
+// fruits.splice(0 , 8);
+// fruits.splice(1, 2);
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
