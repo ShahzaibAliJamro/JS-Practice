1d755631b0fb68cbc970e9852189a06af69265e2
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-13 16:54:27 +0500

    commands added

--- 11-3-24/app.js
+++ 11-3-24/app.js
@@ -1,12 +1,48 @@
 // fruits.splice(2,2);
 // fruits.pop();
 // console.log(fruits);
-const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grape' , 'Strawberry'];
-const input = document.querySelector('#input');
-function shop(){
-    if(fruits.includes(input.value)){
-        console.log(`thik he bhai`);
-    }else{
-        console.log(`maa chudi pari he`);
-    }
-}
+// const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grape', 'Strawberry'];
+// function shop() {
+//     const input = document.querySelector('#input').value;
+//     if (fruits.includes(input)) {
+//         console.log(`thik he bhai`);
+//     } else {
+//         console.log(`maa chudi pari he`);
+//         }
+// }
+
+
+
+// const goats = [
+//     "Pele",
+//     "Diego Maradona",
+//     "Lionel Messi",
+//     "Cristiano Ronaldo",
+//     "Johan Cruyff",
+//     "Franz Beckenbauer",
+//     "Michel Platini",
+//     "Zinedine Zidane",
+//     "Ronaldinho",
+//     "Ronaldo (Brazilian)",
+//     "Ferenc Puskas",
+//     "George Best",
+//     "Alfredo Di Stefano",
+//     "Garrincha",
+//     "Marco van Basten",
+//     "Eusebio",
+//     "Lev Yashin",
+//     "Roberto Baggio",
+//     "Bobby Charlton",
+//     "Zico"
+// ];
+
+// function goatCheck(){
+//     const input = document.querySelector('#input');
+//     const h1 = document.querySelector('h1');
+//     if (goats.includes(input.value)) {
+//         h1.innerHTML = 'The given player is a GOAT Candidate.';
+//     }else{
+//         h1.innerHTML = 'The given player is not a GOAT Candidate.';
+//     }
+//     input.value = ""
+// }
