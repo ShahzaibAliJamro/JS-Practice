f618172394f5dbed3111339e72ff900e3701c289
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-27 19:52:56 +0500

    commands added

---
+++ 27-3-24/app.js
@@ -0,0 +1,34 @@
+// let array = ['apple1','apple2','apple3','apple4','apple5','apple6','apple7','apple8','apple9','apple10','apple11','apple12','apple13','apple14','apple15','apple16','apple17','apple18','apple19','apple20',];
+// let ol = document.querySelector('.ol');
+// printTodo()
+// function printTodo() {
+//     ol.innerHTML = ''
+//     for (let i = 0; i < array.length; i++) {
+//         ol.innerHTML += `<li>${array[i]}
+//         <button class="dlt-button" onclick="dltOne(${i})">Delete</button>
+//         </li>`
+//     }
+// }
+// function deleteAll() {
+//     array = [];
+//     ol.innerHTML = '';
+//     printTodo()
+// };
+// function dltOne(index) {
+//     console.log('hello');
+//     array.splice(index,1);
+//     ol.innerHTML = '';
+//     for (let i = 0; i < array.length; i++) {
+//         ol.innerHTML += `<li>${array[i]}
+//         <button class="dlt-button" onclick="dltOne()">Delete</button>
+//         </li>`
+//     }
+// }
+
+let input1 = document.querySelector('.input1');
+let input2 = document.querySelector('.input2');
+let select = document.querySelector('#select');
+
+function showCalculations()(params) {
+    
+}
