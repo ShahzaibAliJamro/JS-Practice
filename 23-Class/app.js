92e4db8a01146ca4ba588dc77ad4aec64ab73b03
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-05-10 20:42:22 +0500

    commands added

---
+++ 23-Class/app.js
@@ -0,0 +1,22 @@
+// let grade = 'A';
+// switch (grade) {
+//     case 'A':
+//         console.log('malik ache marks aye hein');
+//         break;
+//     case 'B':
+//         console.log('pass hogaya hun');
+//         break;
+//     default:
+//         console.log('parhai karlo');
+//         break;
+// }
+
+// let age = 21;
+// switch (age) {
+//     case age > 17:
+//         console.log('You are old enough');
+//         break;
+//     default:
+//         console.log('Go and use tiktok');
+//         break;
+// }
