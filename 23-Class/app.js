dc8296f1b29cd8b750f20c36af9a6dac87f26ab4
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-05-12 17:11:03 +0500

    commands added

--- 23-Class/app.js
+++ 23-Class/app.js
@@ -11,12 +11,38 @@
 //         break;
 // }
 
-// let age = 21;
-// switch (age) {
-//     case age > 17:
-//         console.log('You are old enough');
-//         break;
-//     default:
-//         console.log('Go and use tiktok');
-//         break;
+
+
+// let str = 'Cristiano'.toLowerCase();
+// console.log(str);
+// let string = str.slice(2);
+// let string2 = str.charAt(str.length-1);
+// console.log(string2);
+// console.log(string);
+
+// let str = 'Lionel Messi'.toLowerCase();
+// let str2 = str.split(' ');
+// console.log(str);
+// let str3 = str2[0].slice(0,1).toUpperCase();
+// let str4 = str2[0].slice(1);
+// let str5 = str2[1].slice(0,1).toUpperCase();
+// let str6 = str2[1].slice(1);
+// let finalStr = str3.concat(str4)+' '+str5.concat(str6);
+// console.log(finalStr);
+
+
+// let text = 'World War II';
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//     }
 // }
+// console.log(text);
+
+// let text = 'Ronaldo';
+// for (let i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 7) === 'Ronaldo') {
+//         text = text.slice(0, i) + 'Messi' + text.slice(i + 12);
+//     }
+// }
+// console.log(text);
