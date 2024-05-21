e4bc2b10fad82392b564a4745f675c03cd85091b
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-05-21 18:19:00 +0500

    commands added

--- 24-Class-Events/app.js
+++ 24-Class-Events/app.js
@@ -1,10 +1,20 @@
-const div = document.querySelector('div');
-const div2 = document.querySelector('.div');
+// const div = document.querySelector('div');
+// const div2 = document.querySelector('.div');
 
-function changeDivColor(event) {
-    event.target.style.backgroundColor = 'red';
-    event.target.style.width = '200px';
-    event.target.style.height = '200px';
-}
+// function changeDivColor(event) {
+//     event.target.style.backgroundColor = 'red';
+//     event.target.style.width = '200px';
+//     event.target.style.height = '200px';
+// }
 
-div.addEventListener('click', changeDivColor);
+// div.addEventListener('click', changeDivColor);
+
+const form = document.querySelector('.form');
+const user = document.querySelector('.user');
+const pass = document.querySelector('.pass');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(user.value);
+    console.log(pass.value);
+})
